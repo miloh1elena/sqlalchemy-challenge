764ca56a8403a25d63f6a50a30f089a4ed8fe6b4
miloh1elena <Administrator@PROF25.xytech.xytechsystems.com>
2023-09-13 20:13:03 -0400

    updated and debugged code with concise comments

--- SurfsUp/Starter_Code/app.py
+++ SurfsUp/Starter_Code/app.py
@@ -3,8 +3,8 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-import sqlalchemy
 from sqlalchemy import func
+from datetime import datetime, timedelta
 
 # Get the current working directory
 current_directory = os.getcwd()
@@ -30,10 +30,6 @@ Base.prepare(db.engine, reflect=True)  # Use the SQLAlchemy engine
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-#################################################
-# Flask Routes
-#################################################
-
 # Define the home route and provide route descriptions.
 @app.route('/')
 def home():
@@ -45,18 +41,15 @@ def home():
     """
     return (
         f"Welcome to the Climate Analysis API!<br/>"
-        "Welcome to the Climate Analysis API!<br/>"
         "Available Routes:<br/>"
         '<a href="/api/v1.0/precipitation" target="_blank">/api/v1.0/precipitation</a> - Precipitation data for the last 12 months<br/>'
         '<a href="/api/v1.0/stations" target="_blank">/api/v1.0/stations</a> - List of weather stations<br/>'
         '<a href="/api/v1.0/tobs" target="_blank">/api/v1.0/tobs</a> - Temperature observations for the last 12 months of the most active station<br/>'
         '<a href="/api/v1.0/start_date" target="_blank">/api/v1.0/start_date</a> - Minimum, average, and maximum temperatures from the start_date to the end of the dataset<br/>'
         '<a href="/api/v1.0/start_date/end_date" target="_blank">/api/v1.0/start_date/end_date</a> - Minimum, average, and maximum temperatures between start_date and end_date'
-)
+    )
 
 # Define the precipitation route and provide a route description.
-from datetime import datetime, timedelta
-
 @app.route('/api/v1.0/precipitation')
 def precipitation():
     """
@@ -105,10 +98,14 @@ def stations():
     return jsonify(stations_list)
 
 # Define the temperature observations route and provide a route description.
-@app.route('/api/v1.0/tobs')
-def tobs():
+@app.route('/api/v1.0/tobs/<start_date>')
+def tobs_start_date_route(start_date):
     """
-    Retrieve and return temperature observations for the last 12 months of the most active station.
+    Retrieve and return temperature observations for the last 12 months of the most active station
+    from start_date to the end of the dataset.
+
+    Args:
+        start_date (str): Start date for temperature analysis.
 
     Returns:
         jsonify: JSON representation of temperature observations.
@@ -122,15 +119,10 @@ def tobs():
         .order_by(func.count(Measurement.station).desc())\
         .first()[0]
 
-    # Calculate the date one year from the most recent date.
-    most_recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
-    most_recent_date = datetime.strptime(most_recent_date, '%Y-%m-%d')  # Convert to datetime object
-    one_year_ago = most_recent_date - timedelta(days=365)
-    one_year_ago_str = one_year_ago.strftime('%Y-%m-%d')
-
-   # Query temperature observations for the last 12 months of the most active station.
-    tobs_data = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == most_active_station)\
-        .filter(Measurement.date >= one_year_ago_str).all()
+    # Query temperature observations for the last 12 months of the most active station from start_date.
+    tobs_data = session.query(Measurement.date, Measurement.tobs)\
+        .filter(Measurement.station == most_active_station)\
+        .filter(Measurement.date >= start_date).all()
 
     # Convert the query results to a list of dictionaries.
     tobs_list = [{"Date": date, "Temperature": tobs} for date, tobs in tobs_data]
@@ -138,7 +130,39 @@ def tobs():
     session.close()  # Close the session
     return jsonify(tobs_list)
 
-# Define the start and end date route and provide a route description.
+# Define start route and provide route descriptions.
+@app.route('/api/v1.0/<start_date>')
+def start_date_route(start_date):
+    """
+    Retrieve and return minimum, average, and maximum temperatures from start_date to the end of the dataset.
+
+    Args:
+        start_date (str): Start date for temperature analysis.
+
+    Returns:
+        jsonify: JSON representation of temperature statistics.
+    """
+    # Create a session using the db object
+    session = Session(db.engine)
+
+    # Query minimum, average, and maximum temperatures from start_date to the end of the dataset.
+    temp_stats = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs))\
+        .filter(Measurement.date >= start_date).all()
+
+    # Convert the query results to a dictionary.
+    temperature_stats = {
+        "Minimum Temperature": temp_stats[0][0],
+        "Average Temperature": temp_stats[0][1],
+        "Maximum Temperature": temp_stats[0][2]
+    }
+
+    # Close the session
+    session.close()
+
+    # Return the temperature statistics as JSON
+    return jsonify(temperature_stats)
+
+# Define start and end route and provide route descriptions.
 @app.route('/api/v1.0/<start_date>/<end_date>')
 def start_end_date_route(start_date, end_date):
     """
@@ -159,14 +183,17 @@ def start_end_date_route(start_date, end_date):
         .filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
 
     # Convert the query results to a dictionary.
-    temp_stats_dict = {
+    temperature_stats = {
         "Minimum Temperature": temp_stats[0][0],
         "Average Temperature": temp_stats[0][1],
         "Maximum Temperature": temp_stats[0][2]
     }
 
-    session.close()  # Close the session
-    return jsonify(temp_stats_dict)
+    # Close the session
+    session.close()
+
+    # Return the temperature statistics as JSON
+    return jsonify(temperature_stats)
 
 # Run the Flask app if this script is executed
 if __name__ == '__main__':
